86d96aeba70aebf0730d49f2362772b529e5639d
harsha <harsha7addanki@gmail.com>
2020-11-11 15:36:18 -0500

    root3

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'PyMLKit',
+    packages = ['src'],
+    version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = '',
+    author = '',
+    author_email = '',
+    url = 'https://github.com/harsha7addanki/PyMLKit',
+    download_url = 'https://github.com/harsha7addanki/PyMLKit/archive/master.zip',
+    keywords = ['PyMLKit'],
+    classifiers = [],
+)
