09f23c5b0f00a4250760d2f50ef08a8c7bf59b8f
harsha <harsha7addanki@gmail.com>
2020-11-11 19:13:18 -0500

    root 780

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,13 @@
 from sys import argv
 import pip
+import venv
 import os
 
 if argv[1] == "new":
     if argv[2] == "project":
-        os.mkdir(argv[3])
+        venv.create(argv[3])
         open(argv[3] + "/main.py","x")
         os.environ["PYMLKIT_PROJ"] = argv[3]
+        pip.main(["install","-t","./" + argv[3] + "/Lib/site-packages","matplotlib","numpy","pandas","scipy","scikit-learn"])
 elif argv[1] == "install":
-    pip.main(["install","-t","/" + os.environ["PYMLKIT_PROJ"],argv[2]])
+    pip.main(["install","-t","./" + os.environ["PYMLKIT_PROJ"],argv[2]])
